ec62d8a1637a25a92c2763606e650fada9d55fd6
S.Anwar.Ahmed <anwarahmed@sourcecodetechnologies.com>
2022-12-21 19:15:34 +0500

    call back hell and inversion of control

--- jsprogramming/jsInBrowser/callbackHell.js
+++ jsprogramming/jsInBrowser/callbackHell.js
@@ -1,24 +1,43 @@
-function loadScript(srcs,callback){
-   let scrpt = document.createElement("script");
-    scrpt.src = srcs;
-    document.body.append(scrpt);
-    scrpt.onload = callback();
-    loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
-        loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
-            loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
-                loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
-                    loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
+// function loadScript(srcs,callback){
+//    let scrpt = document.createElement("script");
+//     scrpt.src = srcs;
+//     document.body.append(scrpt);
+//     scrpt.onload = callback();
+//     loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
+//         loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
+//             loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
+//                 loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
+//                     loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',() => {
         
-                     })
-                })
-            })
-        })    
-    });
+//                      })
+//                 })
+//             })
+//         })    
+//     });
     
+// }
+
+// let hello = () => {
+//     console.log('hello')
+// }
+
+// loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',hello)
+
+let process = (num,callback) => {
+    callback(num);
+    return "Money: "+num+" Account has "+account;
+    
+};
+
+var account = [];
+function saveToAccount(num){
+    return account.push(num);
 }
 
-let hello = () => {
-    console.log('hello')
+function add(a,b,process){
+   return process(a+b,saveToAccount);
 }
 
-loadScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js',hello)
+
+console.log(add(1,2,process));
+
